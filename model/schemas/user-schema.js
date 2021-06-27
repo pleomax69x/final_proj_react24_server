2b64b470d5b884d862a83ec1717ff6037d9cd38e
Anelliya <najdenova.elena@gmail.com>
2021-06-27 16:06:00 +0300

    add bcrypt function, change user schema

--- model/schemas/user-schema.js
+++ model/schemas/user-schema.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema(
   {
@@ -25,6 +26,18 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+userSchema.methods.validPassword = async function (password) {
+  return await bcrypt.compare(String(password), this.password);
+};
 
 const User = model("user", userSchema);
 
