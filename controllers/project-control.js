b5ecf8384579de911c90babd861bbd7ca56180ed
Elena Mazurenko <kingz.bridge@gmail.com>
2021-07-02 15:56:38 +0300

    add checkAccess in projects routes

--- controllers/project-control.js
+++ controllers/project-control.js
@@ -1,24 +1,28 @@
-const Projects = require("../model/project-model");
-const { HttpCode } = require("../helpers/constants");
-const Sprints = require("../model/sprint-model");
+const Projects = require("../model/project-model")
+const Users = require("../model/user-model")
+const { HttpCode } = require("../helpers/constants")
+const Sprints = require("../model/sprint-model")
 
 const create = async (req, res, next) => {
-  const userId = req.user.id;
-  const body = req.body;
+  const userId = req.user.id
+  const body = req.body
   try {
-    const project = await Projects.createProject({ ...body, owner: userId });
-    return res.status(HttpCode.CREATED).json({
+    const project = await Projects.createProject({ ...body, owner: userId })
+    //const member = await Users.addProjectToUser({userId, projectId: project.id })
+    //console.log(member);
+      return res.status(HttpCode.CREATED).json({
       status: "created",
       code: HttpCode.CREATED,
       data: { project },
-    });
+      })
+
   } catch (err) {
     if (err.name === "ValidationError") {
-      err.status = HttpCode.BAD_REQUEST;
+      err.status = HttpCode.BAD_REQUEST
     }
-    next(err);
+    next(err)
   }
-};
+}
 
 const getAll = async (req, res, next) => {
   const userId = req.user.id;
@@ -110,9 +114,40 @@ const patch = async (req, res, next) => {
   }
 };
 
+const checkAccess = async (req, res, next) => {
+  const userId = req.user.id;
+  const projectId = req.params.projectId;
+  try {
+    const project = await Projects.getProjectById(projectId)
+    if (project) {
+      const teammate = await Users.findByProjectsId(userId, projectId)
+      if (teammate) {
+        return res.status(HttpCode.OK).json({
+          status: "success",
+          code: HttpCode.OK,
+          data: { project }
+        })
+      }
+      return res.status(HttpCode.FORBIDDEN).json({
+        status: "error",
+        code: HttpCode.FORBIDDEN,
+        message: "Forbidden",
+      })
+    }
+    return res.status(HttpCode.NOT_FOUND).json({
+      status: "error",
+      code: HttpCode.NOT_FOUND,
+      message: "Not Found",
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
 module.exports = {
   create,
   getAll,
   remove,
   patch,
+  checkAccess,
 };
