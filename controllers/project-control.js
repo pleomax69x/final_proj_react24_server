11c9d7c9f4424c32b6005bb4abdc4f934c929acd
Anelliya <najdenova.elena@gmail.com>
2021-07-03 20:02:37 +0300

    fix fn bugs<=, change some member's rules

--- controllers/project-control.js
+++ controllers/project-control.js
@@ -1,35 +1,33 @@
-const Projects = require("../model/project-model")
-const Users = require("../model/user-model")
-const { HttpCode } = require("../helpers/constants")
-const Sprints = require("../model/sprint-model")
-const User = require("../model/schemas/user-schema")
+const Projects = require("../model/project-model");
+const Users = require("../model/user-model");
+const { HttpCode } = require("../helpers/constants");
+const Sprints = require("../model/sprint-model");
+const User = require("../model/schemas/user-schema");
 
 const create = async (req, res, next) => {
-  const userId = req.user.id
-  const body = req.body
+  const userId = req.user.id;
+  const body = req.body;
   try {
-    const project = await Projects.createProject({ ...body, owner: userId })
+    const project = await Projects.createProject({ ...body, owner: userId });
     //const teammate = await Users.addUserToProject({userId, projectId: project.id })
     //console.log(teammate);
-      return res.status(HttpCode.CREATED).json({
+    return res.status(HttpCode.CREATED).json({
       status: "created",
       code: HttpCode.CREATED,
       data: { project },
-      })
-
+    });
   } catch (err) {
     if (err.name === "ValidationError") {
-      err.status = HttpCode.BAD_REQUEST
+      err.status = HttpCode.BAD_REQUEST;
     }
-    next(err)
+    next(err);
   }
-}
+};
 
 const getAll = async (req, res, next) => {
   const userId = req.user.id;
   const projectsId = req.user.projectsId;
   try {
-    
     const { projects, total, limit, page } = await Projects.getAllProjects(
       userId,
       projectsId,
@@ -56,19 +54,19 @@ const remove = async (req, res, next) => {
   const userId = req.user.id;
   const projectId = req.params.projectId;
   try {
-    const owner = await Projects.isOwner(userId);
+    const owner = await Projects.isOwner(projectId, userId);
+
     if (owner) {
       const projectSprints = await Sprints.getAllSprints(projectId);
       projectSprints.map(async (sprint) => {
-        return await Sprints.removeSprintAndTasks(sprint._id)
-      })
-      
-      const removeUser = await Users.removeUserFromProject(userId, projectId)
+        return await Sprints.removeSprintAndTasks(sprint._id);
+      });
+
+      const removeUser = await Users.removeUserFromProject(userId, projectId);
       if (removeUser) {
-        console.log("User removed from project")
-      }
-      else {
-        console.log("User are not removed")
+        console.log("User removed from project");
+      } else {
+        console.log("User are not removed");
       }
 
       const project = await Projects.removeProject(userId, projectId);
@@ -100,7 +98,7 @@ const patch = async (req, res, next) => {
   const projectId = req.params.projectId;
   const body = req.body;
   try {
-    const owner = await Projects.isOwner(userId);
+    const owner = await Projects.isOwner(projectId, userId);
     if (owner) {
       const project = await Projects.updateProjectName(userId, projectId, body);
       if (project) {
@@ -128,31 +126,31 @@ const checkAccess = async (req, res, next) => {
   const userId = req.user.id;
   const projectId = req.params.projectId;
   try {
-    const project = await Projects.getProjectById(projectId)
+    const project = await Projects.getProjectById(projectId);
     if (project) {
-      const teammate = await Users.findByProjectsId(userId, projectId)
+      const teammate = await Users.findByProjectsId(userId, projectId);
       if (teammate) {
         return res.status(HttpCode.OK).json({
           status: "success",
           code: HttpCode.OK,
-          data: { project }
-        })
+          data: { project },
+        });
       }
       return res.status(HttpCode.FORBIDDEN).json({
         status: "error",
         code: HttpCode.FORBIDDEN,
         message: "Forbidden",
-      })
+      });
     }
     return res.status(HttpCode.NOT_FOUND).json({
       status: "error",
       code: HttpCode.NOT_FOUND,
       message: "Not Found",
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 module.exports = {
   create,
